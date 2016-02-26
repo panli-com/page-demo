00b2fd1176a9fec239ca1a2775fa4fdb15cff7f8
zanjs <root@zanjs.com>
2016-02-26 15:04:35 +0800

    40px

--- 20151228/build/js/main.js
+++ 20151228/build/js/main.js
@@ -1,5 +1,16 @@
 $(function(){
-	
+    //     $.ajax({
+    //         type:"GET",    
+    //         url: "http://api.nnn.li/",
+    //         dataType:"json",
+    //         success:function (result) {
+    //             alert(JSON.stringify(result));
+    //         },
+    //         error:function (result, status) {
+    //         //处理错误 
+    //             alert(result);
+    //         }
+    // });
 });
 /*
  *  ibanner v1.3.3
